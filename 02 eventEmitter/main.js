3366c3be99c9e0c18f3369464549d2a4689af97f
umair-jami <lalaumair92@gmail.com>
2026-05-06 09:57:24 +0500

    updated code

--- 02 eventEmitter/main.js
+++ 02 eventEmitter/main.js
@@ -1,10 +1,22 @@
-const EventEmitter = require('events')
-const emitter = new EventEmitter()
-const greetfunc = (username) => {
-  console.log(`Hello, ${username}!`)
-}
-emitter.on('greet', greetfunc)
-const greetUser = (username) => {
-  emitter.emit('greet', username)
-}
-emitter.on('greet',greetUser)
+const EventEmitter = require("events");
+const emitter = new EventEmitter();
+
+const messageListener = (data) => {
+  console.log("Message received: " + data);
+};
+
+const anotherMessageListener = (data) => {
+  console.log("Another message received: " + data);
+};
+
+emitter.on("message", anotherMessageListener);
+emitter.on("message", messageListener);
+
+emitter.emit("message", "Hello, World!");
+
+console.log(emitter.listeners("message")); // []
+
+// removing (example)
+emitter.removeListener("message", messageListener);
+emitter.removeListener("message",anotherMessageListener)
+console.log(emitter.listeners("message")); // []
